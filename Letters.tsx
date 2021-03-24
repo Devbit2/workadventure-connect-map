bd7677f3bb931c5baeb5e15eeb9328ea14a666f7
Devbit2 <81307840+Devbit2@users.noreply.github.com>
2021-03-24 15:32:33 -0400

    Create Letters.tsx

---
+++ Letters.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Letters" tilewidth="64" tileheight="64" tilecount="26" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_Y_64x64.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_Z_64x64.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_A_64x64.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_B_64x64.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_C_64x64.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_D_64x64.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_E_64x64.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_F_64x64.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_G_64x64.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_H_64x64.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_I_64x64.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_J_64x64.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_K_64x64.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_L_64x64.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_M_64x64.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_N_64x64.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_O_64x64.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_P_64x64.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_Q_64x64.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_R_64x64.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_S_64x64.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_T_64x64.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_U_64x64.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_V_64x64.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_W_64x64.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="C:/Users/Devin/Downloads/Letter_Blocks_01/Letter_Blocks_01/Letter_Blocks_01_Set_3_X_64x64.png"/>
+ </tile>
+</tileset>
